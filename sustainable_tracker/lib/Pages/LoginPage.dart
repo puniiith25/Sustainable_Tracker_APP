fcc8f37ff1654e2912dddef7edceb1b80abfa35b
puniiith25 <puniiith25@gmail.com>
2025-12-04 21:17:32 +0530

    login page

---
+++ sustainable_tracker/lib/Pages/LoginPage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class loginPage extends StatefulWidget {
+  const loginPage({super.key});
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Icon(
+                  FontAwesomeIcons.leaf,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    "Sustainable Shoping",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+
+                      color: Colors.green.withOpacity(0.9),
+                    ),
+                  ),
+                  Text(
+                    'Track your carbon footprint',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: ElevatedButton(onPressed: () {}, child: Text('Login'),
+              style: ButtonStyle(
+                
+              )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- sustainable_tracker/lib/main.dart
+++ sustainable_tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sustainable_tracker/Pages/Home.dart';
+import 'package:sustainable_tracker/Pages/LoginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home(),
+      home: loginPage(),
     );
   }
 }
